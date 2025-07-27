5f79c4ef4944841d41c1a3c5741d6fef5e2f4ea9
raiyannooryrady <raiyannooryrady@gmail.com>
2025-07-27 16:09:45 +0600

    add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import './assets/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <h1>React Dev</h1>
+    <h1 className='text-danger'>React Dev</h1>
     </>
   )
 }
