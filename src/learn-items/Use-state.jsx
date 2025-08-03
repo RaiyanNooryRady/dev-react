0ce9db0ac4092ee309406c5c9981df75d110aa19
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-03 18:16:00 +0600

    updated use state

--- src/learn-items/Use-state.jsx
+++ src/learn-items/Use-state.jsx
@@ -1,16 +1,31 @@
 import { useState } from "react";
 const UseState = () => {
-    const [car, setCar] = useState("Hundai");
+    const [car, setCar] = useState({
+        brand: "Hundai",
+        model: "2025",
+        color: "Red",
+        price: 100000
+    });
     return (
         <div>
             <h1>UseState</h1>
-            <p>MY favorite car is {car}</p>
+            <p>MY favorite car is {car.brand}</p>
             <div className="d-flex gap-2">
-                <button className="btn btn-success" onClick={() => setCar("Volvo")}>Volvo</button>
-                <button className="btn btn-success" onClick={() => setCar("Ena")}>Ena</button>
-                <button className="btn btn-success" onClick={() => setCar("Unique")}>Unique</button>
-                <button className="btn btn-success" onClick={() => setCar("Toyota")}>Toyota</button>
-                <button className="btn btn-success" onClick={() => setCar("Hundai")}>Hundai</button>
+                <button className="btn btn-success" onClick={() => setCar({...car, brand: "Volvo"})}>Volvo</button>
+                <button className="btn btn-success" onClick={() => setCar((prevState)=>{
+                    return {
+                        ...prevState, brand: "Ena"
+                    }
+                })}>Ena</button>
+                <button className="btn btn-success" onClick={() => setCar({...car, brand: "Unique"})}>Unique</button>
+                <button className="btn btn-success" onClick={() => setCar({...car, brand: "Toyota"})}>Toyota</button>
+                <button className="btn btn-success" onClick={() => setCar({...car, brand: "Hundai"})}>Hundai</button>
+                {/* Don't do this as it will replace the whole object */}
+                <button className="btn btn-danger" onClick={() => setCar({brand:"Rejected"})}>Rejected</button>
+                <p>{car.model}</p>
+                <p>{car.color}</p>
+                <p>{car.price}</p>
+             
             </div>
         </div>
     )
