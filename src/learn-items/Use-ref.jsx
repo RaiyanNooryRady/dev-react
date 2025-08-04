a3ab9f20a5d3302edf64f8277ce77f977c95dfd2
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-04 12:58:46 +0600

    use ref hook

--- src/learn-items/Use-ref.jsx
+++ src/learn-items/Use-ref.jsx
@@ -1,15 +1,15 @@
 import { useRef, useEffect, useState } from "react";
 const UseRefExample = () => {
     const [inputValue, setInputValue] = useState("");
-    const [count, setCount] = useState(0);
+    const count = useRef(0);
     useEffect(() => {
-        setCount(count + 1);
-    }, [inputValue]);
+        count.current = count.current + 1;
+    })
     return (
         <>
             <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
             <h1>Use Ref</h1>
-            <p>Render Count: {count}</p>
+            <p>Render Count: {count.current}</p>
         </>
     );
 }
