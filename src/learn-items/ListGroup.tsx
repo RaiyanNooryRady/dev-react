1a08009eb2a86b43846d2342c3460a7ea3d1622c
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-05 22:46:02 +0600

    use-state

--- src/learn-items/ListGroup.tsx
+++ src/learn-items/ListGroup.tsx
@@ -1,22 +1,20 @@
 import React from "react";
-import { MouseEvent } from "react";
-
+import { useState } from "react";
 const ListGroup = () => {
     let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
     //items = [];
-    const handleClick = (event: MouseEvent) => {
-        console.log(event);
-    }
+    const [selectedIndex, setSelectedIndex] = useState(-1);    
     // You can use if statements here, outside the return
     //const message = items.length === 0 ? <p>No items found</p> : null;
     //shorter
     const message = items.length === 0 && <p>No items found</p>
     const showItems = items.map((item, index) => {
+        
         return (
             <li
                 key={item}
-                className="list-group-item"
-                onClick={handleClick}>
+                className={selectedIndex === index ? "list-group-item active cursor-pointer" : "list-group-item cursor-pointer"}
+                onClick={() => setSelectedIndex(index)}>
                 {index} {item}
             </li>
         )
