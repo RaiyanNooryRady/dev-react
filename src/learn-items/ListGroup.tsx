345cc2399029923c9f1378338392e0eb564d8205
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-05 20:12:44 +0600

    handling events

--- src/learn-items/ListGroup.tsx
+++ src/learn-items/ListGroup.tsx
@@ -1,14 +1,25 @@
-
+import React from "react";
+import { MouseEvent } from "react";
 
 const ListGroup = () => {
     let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-     items = [];
+    //items = [];
+    const handleClick = (event: MouseEvent) => {
+        console.log(event);
+    }
     // You can use if statements here, outside the return
     //const message = items.length === 0 ? <p>No items found</p> : null;
     //shorter
     const message = items.length === 0 && <p>No items found</p>
-    const showItems = items.map((item) => {
-        return <li key={item} className="list-group-item" onClick={() => console.log(item)}>{item}</li>
+    const showItems = items.map((item, index) => {
+        return (
+            <li
+                key={item}
+                className="list-group-item"
+                onClick={handleClick}>
+                {index} {item}
+            </li>
+        )
     })
     return (
         <>
