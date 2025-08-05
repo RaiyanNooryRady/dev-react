3822843a6802c05e308243ffd5f752bb4a61955e
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-05 15:13:29 +0600

    conditional statement

--- src/learn-items/ListGroup.jsx
+++ src/learn-items/ListGroup.jsx
@@ -2,19 +2,21 @@
 
 const ListGroup = () => {
     let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-    // items=[];
+   // items = [];
     // You can use if statements here, outside the return
-   
+
 
     return (
         <>
             <h1>List</h1>
+            {
+                items.length === 0 ? <p>No items found</p> : null
+            }
             <ul className="list-group">
                 {
-                    items.length === 0 ? <p>No items found</p> :
-                        items.map((item) => {
-                            return <li key={item} className="list-group-item" onClick={() => console.log(item)}>{item}</li>
-                        })
+                    items.map((item) => {
+                        return <li key={item} className="list-group-item" onClick={() => console.log(item)}>{item}</li>
+                    })
                 }
             </ul>
         </>
