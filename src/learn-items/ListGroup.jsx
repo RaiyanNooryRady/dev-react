aec56925ac7c0f3421c382a194dbca793ba65092
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-05 09:38:17 +0600

    conditional rendering on list group

--- src/learn-items/ListGroup.jsx
+++ src/learn-items/ListGroup.jsx
@@ -4,20 +4,18 @@ const ListGroup = () => {
     let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
     // items=[];
     // You can use if statements here, outside the return
-    let content;
-    if (items.length === 0) {
-        content = <p>No items found</p>;
-    } else {
-        content = items.map((item) => {
-            return <li key={item} className="list-group-item" onClick={() => console.log(item)}>{item}</li>
-        });
-    }
-    
+   
+
     return (
         <>
             <h1>List</h1>
             <ul className="list-group">
-                {content}
+                {
+                    items.length === 0 ? <p>No items found</p> :
+                        items.map((item) => {
+                            return <li key={item} className="list-group-item" onClick={() => console.log(item)}>{item}</li>
+                        })
+                }
             </ul>
         </>
     );
