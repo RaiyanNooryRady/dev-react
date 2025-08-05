be29defc16273162df1718e9601e0a81886da704
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-05 15:22:42 +0600

    conditional renedering techniques

--- src/learn-items/ListGroup.jsx
+++ src/learn-items/ListGroup.jsx
@@ -2,9 +2,11 @@
 
 const ListGroup = () => {
     let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-    // items = [];
+     items = [];
     // You can use if statements here, outside the return
-    const message = items.length === 0 ? <p>No items found</p> : null;
+    //const message = items.length === 0 ? <p>No items found</p> : null;
+    //shorter
+    const message = items.length === 0 && <p>No items found</p>
     const showItems = items.map((item) => {
         return <li key={item} className="list-group-item" onClick={() => console.log(item)}>{item}</li>
     })
